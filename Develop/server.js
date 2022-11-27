6217a60c43aca42e3d4ac3b6ba31277889bf3337
Anmol Gupta <anmol.gupta2180@gmail.com>
2022-11-27 17:11:59 -0500

    I have added code in models index.js whcih helps in creating joining statement

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sync.sequelize({force:true}).then(()=>{
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+  
+})
