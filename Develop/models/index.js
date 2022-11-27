fee8efbca2187120966011ad4203ee5f234debc0
Anmol Gupta <anmol.gupta2180@gmail.com>
2022-11-27 18:47:15 -0500

    I have added code in api routes files which helps in joining and showing data through end points request

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,24 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category hasMany(Product,{
+Category.hasMany(Product,{
   foreignKey:'category_id',
   onDelete:'CASCADE'
 })
 
-Product belongsTo (Category,{
+Product.belongsTo (Category,{
   foreignKey:'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product belongsToMany (Tag, {through:ProductTag,
+Product.belongsToMany (Tag, {through:ProductTag,
 foreignKey:'product_id'})
 
 // Tags belongToMany Products (through ProductTag)
 
-Tag belongsToMany (Product,{
-  through: product_tag,
-  foreignKey:'tah_id'
+Tag.belongsToMany (Product,{
+  through: Product_tag,
+  foreignKey:'tag_id'
 })
 module.exports = {
   Product,
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,25 +3,70 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => { try{
+  const getTag= await Tag.findAll({
+    include:[{model:Product},{model:ProductTag}]
+  })
+
+  res.status(200).json(getTag);
+} catch(err){
+  res.status(500).json(err);
+}
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => { try{
+  const getTagId= await Tag.findByPk(req.params.id ,{
+    include:[{model:Product},{model:ProductTag}]
+  });
+if(!getTagId){
+  res.status(404).json({message:"Invalid Id"};)
+  return;
+} res.status(200).json(getTagId);
+} catch(err){
+  res.status(500).json(err);
+}
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => { try{
+  const tagPost= await Tag.findOrCreate(req.body.tag_name)
+
+  res.status(200).json(tagPost);
+} catch(err){
+  res.status(500).json(err);
+}
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => { try{
+  const tagUpdate= await Tag.update(req.params.id)
+
+  if(!tagUpdate){
+    res.status(404).json({message:"Invalid Id"})
+    return;
+  } res.status(200).json(tagUpdate);
+} catch(err){
+  res.status(500).json(err);
+}
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => { try{
+  const tagDelete= await Tag.destroy({where:{
+    id: req.params.id
+  }})
+
+  if(~tagDelete){
+    res.status(404).json({message:"Invalid Id"})
+    return;
+  } res.status(200).json(tagDelete);
+} catch (err){
+  res.status(500).json(err);
+}
+  
   // delete on tag by its `id` value
 });
 
